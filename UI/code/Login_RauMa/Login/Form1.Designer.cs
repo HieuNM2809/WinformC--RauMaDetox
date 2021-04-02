c941b6078ebc66f0269b290373de84043414aeb4
Lai Khải <56122788+LaiKhai@users.noreply.github.com>
2021-04-02 22:08:07 +0700

    Login-GiaoDien

---
+++ UI/code/Login_RauMa/Login/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Login
+{
+    partial class Login_RauMa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btnSignin = new System.Windows.Forms.Button();
+            this.lnkForGotYourPassword = new System.Windows.Forms.LinkLabel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.lnkCreateAnAccount = new System.Windows.Forms.LinkLabel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.textBox1.Location = new System.Drawing.Point(102, 165);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(316, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "USERNAME";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel1.Location = new System.Drawing.Point(102, 188);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(335, 2);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = global::Login.Properties.Resources.logoRauMa2;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(193, 75);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel3.Location = new System.Drawing.Point(102, 272);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(335, 2);
+            this.panel3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.textBox2.Location = new System.Drawing.Point(102, 249);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(316, 25);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "PASSWORD";
+            // 
+            // panel4
+            // 
+            this.panel4.BackgroundImage = global::Login.Properties.Resources.user1;
+            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel4.Location = new System.Drawing.Point(21, 154);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(55, 47);
+            this.panel4.TabIndex = 6;
+            // 
+            // panel5
+            // 
+            this.panel5.BackgroundImage = global::Login.Properties.Resources.lock1;
+            this.panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel5.Location = new System.Drawing.Point(21, 237);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(55, 47);
+            this.panel5.TabIndex = 7;
+            // 
+            // btnSignin
+            // 
+            this.btnSignin.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnSignin.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignin.ForeColor = System.Drawing.Color.White;
+            this.btnSignin.Location = new System.Drawing.Point(21, 330);
+            this.btnSignin.Name = "btnSignin";
+            this.btnSignin.Size = new System.Drawing.Size(428, 59);
+            this.btnSignin.TabIndex = 8;
+            this.btnSignin.Text = "SIGN IN";
+            this.btnSignin.UseVisualStyleBackColor = false;
+            this.btnSignin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lnkForGotYourPassword
+            // 
+            this.lnkForGotYourPassword.AutoSize = true;
+            this.lnkForGotYourPassword.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkForGotYourPassword.LinkColor = System.Drawing.Color.ForestGreen;
+            this.lnkForGotYourPassword.Location = new System.Drawing.Point(161, 414);
+            this.lnkForGotYourPassword.Name = "lnkForGotYourPassword";
+            this.lnkForGotYourPassword.Size = new System.Drawing.Size(150, 18);
+            this.lnkForGotYourPassword.TabIndex = 9;
+            this.lnkForGotYourPassword.TabStop = true;
+            this.lnkForGotYourPassword.Text = "Forgot your pasword?";
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel6.ForeColor = System.Drawing.Color.ForestGreen;
+            this.panel6.Location = new System.Drawing.Point(128, 452);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(225, 2);
+            this.panel6.TabIndex = 10;
+            // 
+            // lnkCreateAnAccount
+            // 
+            this.lnkCreateAnAccount.AutoSize = true;
+            this.lnkCreateAnAccount.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkCreateAnAccount.LinkColor = System.Drawing.Color.ForestGreen;
+            this.lnkCreateAnAccount.Location = new System.Drawing.Point(173, 484);
+            this.lnkCreateAnAccount.Name = "lnkCreateAnAccount";
+            this.lnkCreateAnAccount.Size = new System.Drawing.Size(129, 18);
+            this.lnkCreateAnAccount.TabIndex = 11;
+            this.lnkCreateAnAccount.TabStop = true;
+            this.lnkCreateAnAccount.Text = "Create an Account";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(982, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(39, 46);
+            this.btnClose.TabIndex = 12;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Login_RauMa
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.White;
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayoutStore = System.Windows.Forms.ImageLayout.Stretch;
+            this.BackgroundImageStore = global::Login.Properties.Resources.panel;
+            this.ClientSize = new System.Drawing.Size(1033, 549);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lnkCreateAnAccount);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.lnkForGotYourPassword);
+            this.Controls.Add(this.btnSignin);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.Glow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Login_RauMa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btnSignin;
+        private System.Windows.Forms.LinkLabel lnkForGotYourPassword;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.LinkLabel lnkCreateAnAccount;
+        private System.Windows.Forms.Button btnClose;
+
+
+    }
+}
+
---
+++ UI/code/Login_RauMa/Login/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Login_RauMa : DevExpress.XtraEditors.XtraForm
+    {
+        public Login_RauMa()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
