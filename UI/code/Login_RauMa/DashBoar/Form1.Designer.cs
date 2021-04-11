4c3ec517918e5c0d87af08afab7911093808614f
Lai Khải <56122788+LaiKhai@users.noreply.github.com>
2021-04-11 16:36:58 +0700

    updateDashBoard

---
+++ UI/code/Login_RauMa/DashBoar/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace DashBoar
+{
+    partial class frmDashboar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDashboar));
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblTaiKhoan = new System.Windows.Forms.Label();
+            this.lblChucVu = new System.Windows.Forms.Label();
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnCaiDat = new System.Windows.Forms.Button();
+            this.btnSanPham = new System.Windows.Forms.Button();
+            this.btnHoaDon = new System.Windows.Forms.Button();
+            this.pnlDecor = new System.Windows.Forms.Panel();
+            this.btnQLTaiKhoan = new System.Windows.Forms.Button();
+            this.picAvatar = new System.Windows.Forms.PictureBox();
+            this.pnlMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picAvatar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.ForeColor = System.Drawing.Color.White;
+            this.lblID.Location = new System.Drawing.Point(123, 82);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(24, 18);
+            this.lblID.TabIndex = 2;
+            this.lblID.Text = "ID";
+            // 
+            // lblTaiKhoan
+            // 
+            this.lblTaiKhoan.AutoSize = true;
+            this.lblTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTaiKhoan.ForeColor = System.Drawing.Color.White;
+            this.lblTaiKhoan.Location = new System.Drawing.Point(64, 108);
+            this.lblTaiKhoan.Name = "lblTaiKhoan";
+            this.lblTaiKhoan.Size = new System.Drawing.Size(148, 24);
+            this.lblTaiKhoan.TabIndex = 3;
+            this.lblTaiKhoan.Text = "Tên Tài Khoản";
+            // 
+            // lblChucVu
+            // 
+            this.lblChucVu.AutoSize = true;
+            this.lblChucVu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChucVu.ForeColor = System.Drawing.Color.White;
+            this.lblChucVu.Location = new System.Drawing.Point(98, 141);
+            this.lblChucVu.Name = "lblChucVu";
+            this.lblChucVu.Size = new System.Drawing.Size(80, 20);
+            this.lblChucVu.TabIndex = 4;
+            this.lblChucVu.Text = "Chức Vụ";
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
+            this.pnlMenu.Controls.Add(this.btnThoat);
+            this.pnlMenu.Controls.Add(this.btnCaiDat);
+            this.pnlMenu.Controls.Add(this.btnSanPham);
+            this.pnlMenu.Controls.Add(this.btnHoaDon);
+            this.pnlMenu.Controls.Add(this.pnlDecor);
+            this.pnlMenu.Controls.Add(this.btnQLTaiKhoan);
+            this.pnlMenu.Controls.Add(this.lblChucVu);
+            this.pnlMenu.Controls.Add(this.lblTaiKhoan);
+            this.pnlMenu.Controls.Add(this.lblID);
+            this.pnlMenu.Controls.Add(this.picAvatar);
+            this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(276, 640);
+            this.pnlMenu.TabIndex = 2;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.FlatAppearance.BorderSize = 0;
+            this.btnThoat.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnThoat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.ForeColor = System.Drawing.Color.White;
+            this.btnThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThoat.Location = new System.Drawing.Point(65, 588);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(144, 31);
+            this.btnThoat.TabIndex = 8;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnCaiDat
+            // 
+            this.btnCaiDat.FlatAppearance.BorderSize = 0;
+            this.btnCaiDat.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnCaiDat.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnCaiDat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCaiDat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCaiDat.ForeColor = System.Drawing.Color.White;
+            this.btnCaiDat.Image = global::DashBoar.Properties.Resources.setting;
+            this.btnCaiDat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCaiDat.Location = new System.Drawing.Point(0, 479);
+            this.btnCaiDat.Name = "btnCaiDat";
+            this.btnCaiDat.Size = new System.Drawing.Size(276, 60);
+            this.btnCaiDat.TabIndex = 7;
+            this.btnCaiDat.Text = "Cài Đặt";
+            this.btnCaiDat.UseVisualStyleBackColor = false;
+            this.btnCaiDat.MouseEnter += new System.EventHandler(this.btnCaiDat_MouseEnter);
+            this.btnCaiDat.MouseLeave += new System.EventHandler(this.btnCaiDat_MouseLeave);
+            // 
+            // btnSanPham
+            // 
+            this.btnSanPham.FlatAppearance.BorderSize = 0;
+            this.btnSanPham.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnSanPham.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnSanPham.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSanPham.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSanPham.ForeColor = System.Drawing.Color.White;
+            this.btnSanPham.Image = global::DashBoar.Properties.Resources.product;
+            this.btnSanPham.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSanPham.Location = new System.Drawing.Point(-1, 387);
+            this.btnSanPham.Name = "btnSanPham";
+            this.btnSanPham.Size = new System.Drawing.Size(277, 60);
+            this.btnSanPham.TabIndex = 6;
+            this.btnSanPham.Text = "Quản Lí Sản Phẩm";
+            this.btnSanPham.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSanPham.UseVisualStyleBackColor = false;
+            this.btnSanPham.MouseEnter += new System.EventHandler(this.btnSanPham_MouseEnter);
+            this.btnSanPham.MouseLeave += new System.EventHandler(this.btnSanPham_MouseLeave);
+            // 
+            // btnHoaDon
+            // 
+            this.btnHoaDon.FlatAppearance.BorderSize = 0;
+            this.btnHoaDon.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnHoaDon.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnHoaDon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHoaDon.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHoaDon.ForeColor = System.Drawing.Color.White;
+            this.btnHoaDon.Image = global::DashBoar.Properties.Resources.bill;
+            this.btnHoaDon.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHoaDon.Location = new System.Drawing.Point(-1, 300);
+            this.btnHoaDon.Name = "btnHoaDon";
+            this.btnHoaDon.Size = new System.Drawing.Size(277, 60);
+            this.btnHoaDon.TabIndex = 5;
+            this.btnHoaDon.Text = "Quản Lí Hóa Đơn";
+            this.btnHoaDon.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnHoaDon.UseVisualStyleBackColor = false;
+            this.btnHoaDon.MouseEnter += new System.EventHandler(this.btnHoaDon_MouseEnter);
+            this.btnHoaDon.MouseLeave += new System.EventHandler(this.btnHoaDon_MouseLeave);
+            // 
+            // pnlDecor
+            // 
+            this.pnlDecor.BackColor = System.Drawing.Color.White;
+            this.pnlDecor.Location = new System.Drawing.Point(47, 175);
+            this.pnlDecor.Name = "pnlDecor";
+            this.pnlDecor.Size = new System.Drawing.Size(200, 4);
+            this.pnlDecor.TabIndex = 3;
+            // 
+            // btnQLTaiKhoan
+            // 
+            this.btnQLTaiKhoan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
+            this.btnQLTaiKhoan.FlatAppearance.BorderSize = 0;
+            this.btnQLTaiKhoan.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnQLTaiKhoan.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnQLTaiKhoan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQLTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQLTaiKhoan.ForeColor = System.Drawing.Color.White;
+            this.btnQLTaiKhoan.Image = global::DashBoar.Properties.Resources.QLTaiKhoan;
+            this.btnQLTaiKhoan.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnQLTaiKhoan.Location = new System.Drawing.Point(-2, 209);
+            this.btnQLTaiKhoan.Name = "btnQLTaiKhoan";
+            this.btnQLTaiKhoan.Size = new System.Drawing.Size(280, 60);
+            this.btnQLTaiKhoan.TabIndex = 0;
+            this.btnQLTaiKhoan.Text = "Quản Lí Tài Khoản";
+            this.btnQLTaiKhoan.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnQLTaiKhoan.UseVisualStyleBackColor = false;
+            this.btnQLTaiKhoan.MouseEnter += new System.EventHandler(this.btnQLTaiKhoan_MouseEnter);
+            this.btnQLTaiKhoan.MouseLeave += new System.EventHandler(this.btnQLTaiKhoan_MouseLeave);
+            // 
+            // picAvatar
+            // 
+            this.picAvatar.BackColor = System.Drawing.Color.Transparent;
+            this.picAvatar.Image = global::DashBoar.Properties.Resources.avatar;
+            this.picAvatar.Location = new System.Drawing.Point(99, 12);
+            this.picAvatar.Name = "picAvatar";
+            this.picAvatar.Size = new System.Drawing.Size(71, 58);
+            this.picAvatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAvatar.TabIndex = 1;
+            this.picAvatar.TabStop = false;
+            // 
+            // frmDashboar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1224, 640);
+            this.Controls.Add(this.pnlMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmDashboar";
+            this.Text = "Rau Má Detox";
+            this.pnlMenu.ResumeLayout(false);
+            this.pnlMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picAvatar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picAvatar;
+        private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblTaiKhoan;
+        private System.Windows.Forms.Label lblChucVu;
+        private System.Windows.Forms.Button btnQLTaiKhoan;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnCaiDat;
+        private System.Windows.Forms.Button btnSanPham;
+        private System.Windows.Forms.Button btnHoaDon;
+        private System.Windows.Forms.Panel pnlDecor;
+    }
+}
+
---
+++ UI/code/Login_RauMa/DashBoar/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DashBoar
+{
+    public partial class frmDashboar : Form
+    {
+        public frmDashboar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQLTaiKhoan_DragEnter(object sender, DragEventArgs e)
+        {
+            btnQLTaiKhoan.Text = "Quản Lí Tài Khoản";
+        }
+
+        private void btnQLTaiKhoan_DragLeave(object sender, EventArgs e)
+        {
+            btnQLTaiKhoan.Text = " ";
+        }
+
+       private void btnCaiDat_MouseEnter(object sender, EventArgs e)
+        {
+            
+            btnCaiDat.ForeColor = Color.FromArgb(39, 174, 96);
+        }
+        private void btnCaiDat_MouseLeave(object sender, EventArgs e)
+        {
+            btnCaiDat.ForeColor = Color.White;
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnQLTaiKhoan_MouseEnter(object sender, EventArgs e)
+        {
+            btnQLTaiKhoan.ForeColor = Color.FromArgb(39, 174, 96);
+        }
+
+        private void btnQLTaiKhoan_MouseLeave(object sender, EventArgs e)
+        {
+            btnQLTaiKhoan.ForeColor = Color.White;
+        }
+
+        private void btnHoaDon_MouseEnter(object sender, EventArgs e)
+        {
+            btnHoaDon.ForeColor = Color.FromArgb(39, 174, 96);
+        }
+
+        private void btnHoaDon_MouseLeave(object sender, EventArgs e)
+        {
+            btnHoaDon.ForeColor = Color.White;
+        }
+
+        private void btnSanPham_MouseEnter(object sender, EventArgs e)
+        {
+            btnSanPham.ForeColor = Color.FromArgb(39, 174, 96);
+        }
+
+        private void btnSanPham_MouseLeave(object sender, EventArgs e)
+        {
+            btnSanPham.ForeColor = Color.White;
+        }
+    }
+}
