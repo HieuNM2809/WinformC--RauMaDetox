cd4d23d5921cc6736282fad6f9652895e6464e7e
leanhkiet071100 <79094686+leanhkiet071100@users.noreply.github.com>
2021-04-12 07:03:51 +0700

    updatfrom nhanvien

---
+++ UI/code/Login_RauMa/QL_NhanVien/FrmThem.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace QL_NhanVien
+{
+    partial class FrmThem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.btnDongY = new System.Windows.Forms.Button();
+            this.lblThongTinNhanVien = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.BackColor = System.Drawing.Color.Green;
+            this.btnHuy.FlatAppearance.BorderSize = 0;
+            this.btnHuy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHuy.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuy.ForeColor = System.Drawing.Color.White;
+            this.btnHuy.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnHuy.Location = new System.Drawing.Point(104, 241);
+            this.btnHuy.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(142, 98);
+            this.btnHuy.TabIndex = 2;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = false;
+            this.btnHuy.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnDongY
+            // 
+            this.btnDongY.BackColor = System.Drawing.Color.Green;
+            this.btnDongY.FlatAppearance.BorderSize = 0;
+            this.btnDongY.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDongY.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDongY.ForeColor = System.Drawing.Color.White;
+            this.btnDongY.Location = new System.Drawing.Point(566, 241);
+            this.btnDongY.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDongY.Name = "btnDongY";
+            this.btnDongY.Size = new System.Drawing.Size(142, 98);
+            this.btnDongY.TabIndex = 3;
+            this.btnDongY.Text = "Đồng ý";
+            this.btnDongY.UseVisualStyleBackColor = false;
+            this.btnDongY.Click += new System.EventHandler(this.btnDongY_Click);
+            // 
+            // lblThongTinNhanVien
+            // 
+            this.lblThongTinNhanVien.AutoSize = true;
+            this.lblThongTinNhanVien.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThongTinNhanVien.ForeColor = System.Drawing.Color.Green;
+            this.lblThongTinNhanVien.Location = new System.Drawing.Point(171, 113);
+            this.lblThongTinNhanVien.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblThongTinNhanVien.Name = "lblThongTinNhanVien";
+            this.lblThongTinNhanVien.Size = new System.Drawing.Size(485, 38);
+            this.lblThongTinNhanVien.TabIndex = 18;
+            this.lblThongTinNhanVien.Text = "BẠN CÓ MUỐN THÊM NHÂN VIÊN?";
+            // 
+            // FrmThem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblThongTinNhanVien);
+            this.Controls.Add(this.btnDongY);
+            this.Controls.Add(this.btnHuy);
+            this.Name = "FrmThem";
+            this.Text = "FrmThem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Button btnDongY;
+        private System.Windows.Forms.Label lblThongTinNhanVien;
+    }
+}
