8b0739e5f2711f434186b615a99c7a8894ecce22
kingcartoon0445 <79097005+kingcartoon0445@users.noreply.github.com>
2021-04-25 18:33:01 +0700

    update

--- form_NhanVien(update)/form_NhanVien/Properties/Resources.Designer.cs
+++ form_NhanVien(update)/form_NhanVien/Properties/Resources.Designer.cs
@@ -109,5 +109,25 @@ namespace form_NhanVien.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Xoa {
+            get {
+                object obj = ResourceManager.GetObject("Xoa", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Xoa1 {
+            get {
+                object obj = ResourceManager.GetObject("Xoa1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
