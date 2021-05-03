d14b1c2d499a9d3354932dd2ee1da05e14a499f0
leanhkiet071100 <79094686+leanhkiet071100@users.noreply.github.com>
2021-05-03 18:36:08 +0700

    update form nhan vien

--- UI/code/Login_RauMa/DashBoar/FrmDangKiTaiKhoan.cs
+++ UI/code/Login_RauMa/DashBoar/FrmDangKiTaiKhoan.cs
@@ -21,33 +21,33 @@ namespace DashBoar
 
         private void btnThem_Click(object sender, EventArgs e)
         {
-            //tao doi tuong ket noi
-            SqlConnection conn = new SqlConnection(strConnection);
+            ////tao doi tuong ket noi
+            //SqlConnection conn = new SqlConnection(strConnection);
 
-            //mo ket noi
-            conn.Open();
+            ////mo ket noi
+            //conn.Open();
 
-            //Câu truy vấn thêm đối tượng sinh viên mới
-            string sqlFomat = "INSERT INTO CT_PhanQuyen(ID_NV, TaiKhoan, MatKhau, LoaiQuyen) VALUES ('{0}', N'{1}', '{2}', '{3}', '{4}')";
+            ////Câu truy vấn thêm đối tượng sinh viên mới
+            //string sqlFomat = "INSERT INTO CT_PhanQuyen(ID_NV, TaiKhoan, MatKhau, LoaiQuyen) VALUES ('{0}', N'{1}', '{2}', '{3}', '{4}')";
 
-            //Hoàn thiện câu truy vấn
-            string sql = string.Format(sqlFomat, txtMaNhanVien.Text, txtDangNhap.Text, txtMatKhau.Text, cbbChứcDanh.Text);
+            ////Hoàn thiện câu truy vấn
+            //string sql = string.Format(sqlFomat, txtMaNhanVien.Text, txtDangNhap.Text, txtMatKhau.Text, cbbChứcDanh.Text);
 
-            //tạo đối tượng thực thi
-            SqlCommand cmd = new SqlCommand(sql, conn);
+            ////tạo đối tượng thực thi
+            //SqlCommand cmd = new SqlCommand(sql, conn);
 
-            int count = cmd.ExecuteNonQuery();
+            //int count = cmd.ExecuteNonQuery();
 
-            if (count > 0)
-            {
-                // Thêm thành công
-                MessageBox.Show("Thêm thành công", "Kết quả", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else
-            {
-                // Thêm thất bại
-                MessageBox.Show("Thêm thất bại", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            //if (count > 0)
+            //{
+            //    // Thêm thành công
+            //    MessageBox.Show("Thêm thành công", "Kết quả", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //}
+            //else
+            //{
+            //    // Thêm thất bại
+            //    MessageBox.Show("Thêm thất bại", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
 
 
 
