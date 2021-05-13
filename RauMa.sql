3f8e274fc11c3d536ef0f5cd733549764f82dee5
Lai Khải <56122788+LaiKhai@users.noreply.github.com>
2021-05-13 22:47:58 +0700

    update_13_05_3

--- RauMa.sql
+++ RauMa.sql
@@ -13,14 +13,14 @@ MoTa nvarchar(50),
 Hinh image,
 TrangThai bit default 1
 )
-
+go
 create table LoaiSP
 (
 MaLoaiSp char(3) primary key not null,
 LoaiSP nvarchar(20),
 TrangThai bit default 1
 )
-
+go
 create table NhanVien
 (
 ID_NV char(3) not null,
@@ -35,6 +35,7 @@ MatKhau varchar(100),
 Email varchar(100),
 TrangThai Bit default 1,
 )
+go
 create table CT_PhanQuyen
 (
 	ID_NV char(3),
@@ -43,18 +44,20 @@ create table CT_PhanQuyen
 	LoaiQuyen varchar(50),
 	TrangThai Bit default 1
 )
+go
 create table PhanQuyen
 (
 	ID_quyen char(3) not null,
 	LoaiQuyen varchar(50)
 )
-
+go
 create table HoaDon
 (
 	ID_HoaDon char(3) not null,
 	ID_NV char(3),
 	NgayLapHoaDon datetime,
 )
+go
 create table CT_HoaDon
 (
 	ID_HoaDon char(3),
@@ -64,34 +67,35 @@ create table CT_HoaDon
 	ChietKhau money,
 	TongTien money
 )
-
+go
 
 
 
 --Khóa Chính, Khóa Ngoại 
 alter table SanPham
 add constraint FK_SanPham_LoaiSanPham foreign key (MaLoaiSP) references LoaiSP(MaLoaiSP)
-
+go
 alter table NhanVien
 add constraint PK_NhanVien primary key (ID_NV)
-
+go
 alter table PhanQuyen
 add constraint PK_PhanQuyen primary key (ID_quyen)
-
+go
 alter table HoaDon
 add constraint PK_HoaDon primary key (ID_HoaDon)
-
+go
 alter table CT_PhanQuyen
 add constraint FK_CTPhanQuyen_PhanQuyen foreign key (ID_quyen) references PhanQuyen(ID_quyen)
-
+go
 alter table CT_PhanQuyen
 add constraint FK_CTPhanQuyen_NhanVien foreign key (ID_NV) references NhanVien(ID_NV)
-
+go
 alter table CT_HoaDon
 add constraint FK_CTHoaDon_HoaDon foreign key (ID_HoaDon) references HoaDon(ID_HoaDon)
-
+go
 alter table CT_HoaDon
 add constraint FK_CTHoaDon_SanPham foreign key (MaSp) references SanPham(MaSp)
-
+go
 alter table HoaDon
 add constraint FK_HoaDon_NhanVien foreign key (ID_NV) references NhanVien(ID_NV)
+go
