53eaef38c7d9a9fdd3ab08892d3e9638c4b03403
leanhkiet071100 <79094686+leanhkiet071100@users.noreply.github.com>
2021-05-15 13:56:58 +0700

    1

--- UI/code/Login_RauMa/DAO/NhanVienDAO.cs
+++ UI/code/Login_RauMa/DAO/NhanVienDAO.cs
@@ -14,43 +14,50 @@ namespace DAO
 
         private SqlConnection _conn = new SqlConnection(_connectionString);
 
+        ql_raumaEntities _qlNhanViênntities = new ql_raumaEntities();
+
         public List<NhanVienDTO> LayDSNhanVien()
         {
-            List<NhanVienDTO> lstNhanVien = new List<NhanVienDTO>();
+            //List<NhanVienDTO> lstNhanVien = new List<NhanVienDTO>();
 
-            _conn.Open();
+            //_conn.Open();
 
-            string sql = "SELECT * FROM NhanVien WHERE TrangThai = 1";
+            //string sql = "SELECT * FROM NhanVien WHERE TrangThai = 1";
 
-            SqlCommand cmd = new SqlCommand(sql, _conn);
+            //SqlCommand cmd = new SqlCommand(sql, _conn);
 
-            SqlDataReader sdr = cmd.ExecuteReader();
+            //SqlDataReader sdr = cmd.ExecuteReader();
 
-            if (sdr.HasRows)
-            {
-                while (sdr.Read())
-                {
-                    NhanVienDTO nv = new NhanVienDTO
-                    {
-                        ID_NV = sdr.GetString(0),
-                        HoTen = sdr.GetString(1),
-                        NgaySinh = sdr.GetDateTime(2),
-                        GioiTinh = sdr.GetString(3),
-                        ChucDanh = sdr.GetString(4),
-                        LoaiNV = sdr.GetString(5),
-                        SDT = sdr.GetString(6),
-                        TaiKhoan = sdr.GetString(7),
-                        MatKhau = sdr.GetString(8),
-                        Email = sdr.GetString(9),
-                    };
+            //if (sdr.HasRows)
+            //{
+            //    while (sdr.Read())
+            //    {
+            //        NhanVienDTO nv = new NhanVienDTO
+            //        {
+            //            ID_NV = sdr.GetString(0),
+            //            HoTen = sdr.GetString(1),
+            //            NgaySinh = sdr.GetDateTime(2),
+            //            GioiTinh = sdr.GetString(3),
+            //            ChucDanh = sdr.GetString(4),
+            //            LoaiNV = sdr.GetString(5),
+            //            SDT = sdr.GetString(6),
+            //            TaiKhoan = sdr.GetString(7),
+            //            MatKhau = sdr.GetString(8),
+            //            Email = sdr.GetString(9),
+            //        };
 
-                    lstNhanVien.Add(nv);
-                }
-            }
+            //        lstNhanVien.Add(nv);
+            //    }
+            //}
 
-            _conn.Close();
+            //_conn.Close();
+            
+            List<NhanVienDTO> lsnhanvien   = new  List<NhanVienDTO>();
 
-            return lstNhanVien;
+            lsnhanvien = _qlNhanViênntities.NhanViens.Where(v => v.TrangThai == true).Select(u => new NhanVienDTO {ID_NV =u.IDNV, HoTen = u.HoTen, NgaySinh = u.NgaySinh.Value,
+                       GioiTinh =u.GioiTinh, ChucDanh = u.ChucDanh, LoaiNV = u.LoaiNV, TaiKhoan=u.TaiKhoan, MatKhau = u.MatKhau, SDT=u.SDT, Email=u.Email}).ToList();
+
+            return lsnhanvien;
         }
 
         public bool KiemTraNhanVien(string IDNV)
