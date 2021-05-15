6163aa82189c61dff1fcd9c254c968b573146381
Lai Khải <56122788+LaiKhai@users.noreply.github.com>
2021-05-15 18:17:17 +0700

    update_1505_5

--- UI/code/Login_RauMa/DashBoar/frmHoaDon.Designer.cs
+++ UI/code/Login_RauMa/DashBoar/frmHoaDon.Designer.cs
@@ -31,8 +31,15 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHoaDon));
             this.panel1 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colIDHoaDon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTenSp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSoLuong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDonGia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTongTien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.picNhanVien = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNhanVien)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -46,7 +53,14 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colIDHoaDon,
+            this.colTenSp,
+            this.colSoLuong,
+            this.colDonGia,
+            this.colTongTien});
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
@@ -55,12 +69,52 @@
             this.dataGridView1.Size = new System.Drawing.Size(999, 247);
             this.dataGridView1.TabIndex = 0;
             // 
+            // colIDHoaDon
+            // 
+            this.colIDHoaDon.HeaderText = "ID";
+            this.colIDHoaDon.MinimumWidth = 6;
+            this.colIDHoaDon.Name = "colIDHoaDon";
+            // 
+            // colTenSp
+            // 
+            this.colTenSp.HeaderText = "Tên Sản Phẩm";
+            this.colTenSp.MinimumWidth = 6;
+            this.colTenSp.Name = "colTenSp";
+            // 
+            // colSoLuong
+            // 
+            this.colSoLuong.HeaderText = "Số Lượng";
+            this.colSoLuong.MinimumWidth = 6;
+            this.colSoLuong.Name = "colSoLuong";
+            // 
+            // colDonGia
+            // 
+            this.colDonGia.HeaderText = "Đơn Giá";
+            this.colDonGia.MinimumWidth = 6;
+            this.colDonGia.Name = "colDonGia";
+            // 
+            // colTongTien
+            // 
+            this.colTongTien.HeaderText = "Tổng Tiền";
+            this.colTongTien.MinimumWidth = 6;
+            this.colTongTien.Name = "colTongTien";
+            // 
+            // picNhanVien
+            // 
+            this.picNhanVien.BackColor = System.Drawing.Color.White;
+            this.picNhanVien.Location = new System.Drawing.Point(23, 12);
+            this.picNhanVien.Name = "picNhanVien";
+            this.picNhanVien.Size = new System.Drawing.Size(132, 144);
+            this.picNhanVien.TabIndex = 1;
+            this.picNhanVien.TabStop = false;
+            // 
             // frmHoaDon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
             this.ClientSize = new System.Drawing.Size(999, 603);
+            this.Controls.Add(this.picNhanVien);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -68,6 +122,7 @@
             this.Text = "frmHoaDon";
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNhanVien)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -76,5 +131,11 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colIDHoaDon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTenSp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSoLuong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDonGia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTongTien;
+        private System.Windows.Forms.PictureBox picNhanVien;
     }
 }
