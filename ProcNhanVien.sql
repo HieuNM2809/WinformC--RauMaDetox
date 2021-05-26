f23a126e0a7db915fe7674bcd746910fbd1c4dce
leanhkiet071100 <79094686+leanhkiet071100@users.noreply.github.com>
2021-05-26 22:36:19 +0700

    update_26_05

---
+++ ProcNhanVien.sql
@@ -0,0 +1,55 @@
+CREATE PROC ThemNV
+(	
+	@idnv nvarchar(3),
+	@hoten nvarchar(50),
+	@ngaysinh datetime,
+	@gioitinh nvarchar(10),
+	@chucdanh nvarchar(30),
+	@loainv nvarchar(20),
+	@sdt char(10),
+	@taikhoan varchar(100),
+	@matkhau varchar(100),
+	@Email varchar(100) 
+)
+AS 
+BEGIN
+	INSERT INTO NhanVien(IDNV,HoTen, NgaySinh, GioiTinh, ChucDanh, LoaiNV, SDT, TaiKhoan, MatKhau, Email, TrangThai) 
+	VALUES (@idnv, @hoten, @ngaysinh, @gioitinh, @chucdanh, @loainv, @sdt, @taikhoan, @matkhau, @Email, 1)
+END
+
+GO 
+
+CREATE PROC XaoNV
+(
+	@idnv nvarchar(3)
+)
+AS
+BEGIN
+	UPDATE NhanVien
+	SET TrangThai = 0
+	WHERE IDNV = @idnv
+END
+
+GO
+CREATE PROC CapNhatNV
+(
+	@idnv nvarchar(3),
+	@hoten nvarchar(50),
+	@ngaysinh datetime,
+	@gioitinh nvarchar(10),
+	@chucdanh nvarchar(30),
+	@loainv nvarchar(20),
+	@sdt char(10),
+	@taikhoan varchar(100),
+	@matkhau varchar(100),
+	@Email varchar(100) 
+)
+AS
+BEGIN
+	UPDATE NhanVien
+	SET IDNV = @idnv, HoTen = @hoten, NgaySinh = @ngaysinh, GioiTinh = @gioitinh,
+		ChucDanh = @chucdanh, LoaiNV = @loainv, SDT = @sdt, TaiKhoan = @taikhoan,
+		MatKhau = @matkhau, Email = @Email
+	WHERE IDNV = @idnv
+END
+
