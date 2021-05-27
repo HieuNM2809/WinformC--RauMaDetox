6957062d3de5f574d4315c4b2667289b7d78c419
kingcartoon0445 <dauphong0445@gmail.com>
2021-05-27 10:35:36 +0700

    update_27/5

--- UI/code/Login_RauMa/DashBoar/from_Setting.Designer.cs
+++ UI/code/Login_RauMa/DashBoar/from_Setting.Designer.cs
@@ -34,13 +34,15 @@ namespace DashBoar
             this.label2 = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
             this.tbx_maumau = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_xacnhan = new System.Windows.Forms.Button();
+            this.btn_huy = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -99,18 +101,15 @@ namespace DashBoar
             this.groupBox1.Size = new System.Drawing.Size(336, 195);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Giao diện";
             // 
-            // button1
+            // tbx_maumau
             // 
-            this.button1.BackColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(144, 141);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 27);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Chọn màu";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.tbx_maumau.ForeColor = System.Drawing.Color.Yellow;
+            this.tbx_maumau.Location = new System.Drawing.Point(268, 143);
+            this.tbx_maumau.Name = "tbx_maumau";
+            this.tbx_maumau.Size = new System.Drawing.Size(34, 22);
+            this.tbx_maumau.TabIndex = 6;
             // 
             // label3
             // 
@@ -121,13 +120,16 @@ namespace DashBoar
             this.label3.TabIndex = 5;
             this.label3.Text = "Màu nền";
             // 
-            // tbx_maumau
+            // button1
             // 
-            this.tbx_maumau.ForeColor = System.Drawing.Color.Yellow;
-            this.tbx_maumau.Location = new System.Drawing.Point(268, 143);
-            this.tbx_maumau.Name = "tbx_maumau";
-            this.tbx_maumau.Size = new System.Drawing.Size(34, 22);
-            this.tbx_maumau.TabIndex = 6;
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(144, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Chọn màu";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBox2
             // 
@@ -140,49 +142,85 @@ namespace DashBoar
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Thông tin đội sản xuất";
             // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.White;
-            this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(74, 30);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 43);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Liên hệ";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(204, 30);
+            this.button3.Location = new System.Drawing.Point(30, 30);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 43);
+            this.button3.Size = new System.Drawing.Size(114, 43);
             this.button3.TabIndex = 1;
             this.button3.Text = "Infor";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button3.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // label4
             // 
-            this.button4.BackColor = System.Drawing.Color.Red;
-            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(419, 319);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(52, 40);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "X";
-            this.button4.UseVisualStyleBackColor = false;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(166, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 39);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Cài đặt";
+            // 
+            // btn_xacnhan
+            // 
+            this.btn_xacnhan.BackColor = System.Drawing.Color.White;
+            this.btn_xacnhan.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_xacnhan.FlatAppearance.BorderSize = 0;
+            this.btn_xacnhan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_xacnhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_xacnhan.Image = global::DashBoar.Properties.Resources.xacnhan1;
+            this.btn_xacnhan.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_xacnhan.Location = new System.Drawing.Point(397, 302);
+            this.btn_xacnhan.Name = "btn_xacnhan";
+            this.btn_xacnhan.Size = new System.Drawing.Size(135, 57);
+            this.btn_xacnhan.TabIndex = 7;
+            this.btn_xacnhan.Text = "Xác nhận";
+            this.btn_xacnhan.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_xacnhan.UseVisualStyleBackColor = false;
+            // 
+            // btn_huy
+            // 
+            this.btn_huy.BackColor = System.Drawing.Color.White;
+            this.btn_huy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_huy.FlatAppearance.BorderSize = 0;
+            this.btn_huy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_huy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_huy.ForeColor = System.Drawing.Color.Black;
+            this.btn_huy.Image = global::DashBoar.Properties.Resources.exits3;
+            this.btn_huy.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_huy.Location = new System.Drawing.Point(397, 221);
+            this.btn_huy.Name = "btn_huy";
+            this.btn_huy.Size = new System.Drawing.Size(135, 57);
+            this.btn_huy.TabIndex = 6;
+            this.btn_huy.Text = "Hủy";
+            this.btn_huy.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_huy.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(188, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 43);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Liên hệ";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // from_Setting
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
-            this.ClientSize = new System.Drawing.Size(506, 399);
-            this.Controls.Add(this.button4);
+            this.ClientSize = new System.Drawing.Size(554, 399);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_xacnhan);
+            this.Controls.Add(this.btn_huy);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -193,6 +231,7 @@ namespace DashBoar
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -209,6 +248,8 @@ namespace DashBoar
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btn_huy;
+        private System.Windows.Forms.Button btn_xacnhan;
+        private System.Windows.Forms.Label label4;
     }
 }
