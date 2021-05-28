c48b0bfb9062481dd1074ffd46afc04ee102444f
kingcartoon0445 <dauphong0445@gmail.com>
2021-05-29 00:25:18 +0700

    Revert "Merge branch 'main' of https://github.com/LaiKhai/WinformC--RauMaDetox into main"
    This reverts commit f5b2329a4592d95c3f043e6702f82d8d3c191704, reversing
    changes made to fcc303a00090d0189ce6dfb21d52c41683501d85.

--- UI/code/Login_RauMa/DashBoar/frmChiTietHoaDon.cs
+++ UI/code/Login_RauMa/DashBoar/frmChiTietHoaDon.cs
@@ -14,18 +14,11 @@ namespace DashBoar
 {
     public partial class frmChiTietHoaDon : Form
     {
-        ChiTietHoaDonBUS cthd = new ChiTietHoaDonBUS();
-        HoaDonBUS hd = new HoaDonBUS();
-        ChiTietHoaDonDTO chitiethoadon = new ChiTietHoaDonDTO();
+        BUS_sanpham sp = new BUS_sanpham();
         public frmChiTietHoaDon()
         {
             InitializeComponent();
         }
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         private void frmChiTietHoaDon_load(object sender, EventArgs e)
         {
             cbbTenSP.DataSource = sp.lstViewSP();
@@ -35,174 +28,7 @@ namespace DashBoar
         }
         private void button1_Click(object sender, EventArgs e)
         {
-=======
-        private int count = 0;
-        private void frmChiTietHoaDon_Load(object sender, EventArgs e)
-        {
-            cbbTenSP.DataSource = cthd.layDSSP();
-            cbbTenSP.DisplayMember = "Tensp";
-            cbbTenSP.ValueMember = "Masp";
-=======
-        private int count = 0;
-        private void frmChiTietHoaDon_Load(object sender, EventArgs e)
-        {
-=======
-        private int count = 0;
-        private void frmChiTietHoaDon_Load(object sender, EventArgs e)
-        {
->>>>>>> Stashed changes
-            cbbTenSP.DataSource = cthd.layDSSP();
-            cbbTenSP.DisplayMember = "Tensp";
-            cbbTenSP.ValueMember = "Masp";
 
-        }
-
-        private void btnXacNhan_Click(object sender, EventArgs e)
-<<<<<<< Updated upstream
-        {
-            if(String.IsNullOrEmpty(txtChietKhau.Text)||cbbTenSP.SelectedValue==null||numbrSoluong.Value<0)
-            {
-                MessageBox.Show(Constants.ERR_REQUIRED_2, "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            else
-            { 
-                count++;
-                ListViewItem lst = new ListViewItem(count.ToString());
-                lst.SubItems.Add(cbbTenSP.SelectedValue.ToString());
-                
-                lst.SubItems.Add(cbbTenSP.Text);
-                lst.SubItems.Add(numbrSoluong.Value.ToString());
-                lst.SubItems.Add(cbbGiaTien.Text);
-                lst.SubItems.Add(txtChietKhau.Text);
-=======
-        private int count = 0;
-        private void frmChiTietHoaDon_Load(object sender, EventArgs e)
-        {
-            cbbTenSP.DataSource = cthd.layDSSP();
-            cbbTenSP.DisplayMember = "Tensp";
-            cbbTenSP.ValueMember = "Masp";
->>>>>>> Stashed changes
-=======
-        {
-            if(String.IsNullOrEmpty(txtChietKhau.Text)||cbbTenSP.SelectedValue==null||numbrSoluong.Value<0)
-            {
-                MessageBox.Show(Constants.ERR_REQUIRED_2, "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            else
-            { 
-                count++;
-                ListViewItem lst = new ListViewItem(count.ToString());
-                lst.SubItems.Add(cbbTenSP.SelectedValue.ToString());
-                
-                lst.SubItems.Add(cbbTenSP.Text);
-                lst.SubItems.Add(numbrSoluong.Value.ToString());
-                lst.SubItems.Add(cbbGiaTien.Text);
-                lst.SubItems.Add(txtChietKhau.Text);
->>>>>>> Stashed changes
-
-                lstDanhSachSP.Items.Add(lst); 
-            }    
-        }
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-
-<<<<<<< Updated upstream
-        private void cbbTenSP_SelectedValueChanged(object sender, EventArgs e)
-        {
-            cbbGiaTien.DataSource = cthd.lstViewSP(cbbTenSP.SelectedValue.ToString());
-            cbbGiaTien.DisplayMember = "DonGia";
-            cbbGiaTien.ValueMember = "Masp";
-        }
-
-        private void btnXacNhan_Click(object sender, EventArgs e)
-        {
-            if(String.IsNullOrEmpty(txtChietKhau.Text)||cbbTenSP.SelectedValue==null||numbrSoluong.Value<0)
-            {
-                MessageBox.Show(Constants.ERR_REQUIRED_2, "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            else
-            { 
-                count++;
-                ListViewItem lst = new ListViewItem(count.ToString());
-                lst.SubItems.Add(cbbTenSP.SelectedValue.ToString());
-                
-                lst.SubItems.Add(cbbTenSP.Text);
-                lst.SubItems.Add(numbrSoluong.Value.ToString());
-                lst.SubItems.Add(cbbGiaTien.Text);
-                lst.SubItems.Add(txtChietKhau.Text);
-
-                lstDanhSachSP.Items.Add(lst); 
-            }    
-        }
->>>>>>> Stashed changes
-
-=======
-        private int count = 0;
-        private void frmChiTietHoaDon_Load(object sender, EventArgs e)
-        {
-            cbbTenSP.DataSource = cthd.layDSSP();
-            cbbTenSP.DisplayMember = "Tensp";
-            cbbTenSP.ValueMember = "Masp";
-=======
-        private void btnXacNhan_Click(object sender, EventArgs e)
-        {
-            if(String.IsNullOrEmpty(txtChietKhau.Text)||cbbTenSP.SelectedValue==null||numbrSoluong.Value<0)
-            {
-                MessageBox.Show(Constants.ERR_REQUIRED_2, "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            else
-            { 
-                count++;
-                ListViewItem lst = new ListViewItem(count.ToString());
-                lst.SubItems.Add(cbbTenSP.SelectedValue.ToString());
-                
-                lst.SubItems.Add(cbbTenSP.Text);
-                lst.SubItems.Add(numbrSoluong.Value.ToString());
-                lst.SubItems.Add(cbbGiaTien.Text);
-                lst.SubItems.Add(txtChietKhau.Text);
-
-                lstDanhSachSP.Items.Add(lst); 
-            }    
-        }
->>>>>>> Stashed changes
-
-        private void cbbTenSP_SelectedValueChanged(object sender, EventArgs e)
-        {
-            cbbGiaTien.DataSource = cthd.lstViewSP(cbbTenSP.SelectedValue.ToString());
-            cbbGiaTien.DisplayMember = "DonGia";
-            cbbGiaTien.ValueMember = "Masp";
-        }
-
-        private void btnXacNhan_Click(object sender, EventArgs e)
-        {
-            if(String.IsNullOrEmpty(txtChietKhau.Text)||cbbTenSP.SelectedValue==null||numbrSoluong.Value<0)
-            {
-                MessageBox.Show(Constants.ERR_REQUIRED_2, "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            else
-            { 
-                count++;
-                ListViewItem lst = new ListViewItem(count.ToString());
-                lst.SubItems.Add(cbbTenSP.SelectedValue.ToString());
-                
-                lst.SubItems.Add(cbbTenSP.Text);
-                lst.SubItems.Add(numbrSoluong.Value.ToString());
-                lst.SubItems.Add(cbbGiaTien.Text);
-                lst.SubItems.Add(txtChietKhau.Text);
-
-                lstDanhSachSP.Items.Add(lst); 
-            }    
-        }
-
->>>>>>> Stashed changes
-=======
-
->>>>>>> Stashed changes
-        private void cbbTenSP_SelectedValueChanged(object sender, EventArgs e)
-        {
-            cbbGiaTien.DataSource = cthd.lstViewSP(cbbTenSP.SelectedValue.ToString());
-            cbbGiaTien.DisplayMember = "DonGia";
-            cbbGiaTien.ValueMember = "Masp";
         }
     }
 }
